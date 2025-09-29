2c11543f0f965ae2c6ef34e946efa29f54ceae6d
aravindh-jawahar <j.aravindh@zohomail.com>
2025-09-30 01:02:05 +0530

    Step:1 Comma seperated numbers to get sum

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 class StringCalculator
-  attr_accessor :numbers
-
-  def initialize(numbers = nil)
-    @numbers = numbers
-  end
-
   def add(numbers)
-    return if numbers.blank?
+    return 0 if numbers.nil? || numbers.strip.empty?
 
-    numbers.split(',').sum
+    numbers.split(',').map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,8 +1,36 @@
-
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:subject) { described_class.new.add(numbers) }
+  let(:numbers) { "1,2" }
+
   describe '#add' do
-    
+    context 'when numbers are empty' do
+      let(:numbers) { "" }
+      it 'returns 0' do
+        expect(subject).to eq(0)
+      end
+    end
+
+    context 'when numbers are nil' do
+      let(:numbers) { nil }
+      it 'returns 0' do
+        expect(subject).to eq(0)
+      end
+    end
+
+    context 'when numbers are a single number' do
+      let(:numbers) { "5" }
+      it 'returns the number itself' do
+        expect(subject).to eq(5)
+      end
+    end
+
+    context 'when numbers are a comma-separated string' do
+      let(:numbers) { "1,2" }
+      it 'returns the sum of the numbers' do
+        expect(subject).to eq(3)
+      end
+    end
   end
 end
